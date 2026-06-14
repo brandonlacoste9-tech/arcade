40cdaab9f3117c0fc337e499296e8d472ace82c0
Brandon Lacoste <brandonlacoste9@gmail.com>
2026-06-14 00:54:00 -0400

    Added Traffic Bot and Google SEO Sitemap Generator

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -7,11 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const SITEMAP_FILE = path.join(__dirname, 'public', 'sitemap.xml');
+// Use the production domain in the sitemap for Google
 const BASE_URL = 'https://hellyeah-games.com';
 
 const generateSitemap = () => {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url>
     <loc>${BASE_URL}/</loc>
     <changefreq>daily</changefreq>
@@ -30,17 +32,24 @@ const generateSitemap = () => {
 `;
 
   gamesData.forEach(game => {
+    // Escape ampersands and special characters in URLs for valid XML
+    const safeUrl = (url) => url ? url.replace(/&/g, '&amp;') : '';
+    
     xml += `  <url>
     <loc>${BASE_URL}/game/${game.id}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.6</priority>
+    <image:image>
+      <image:loc>${safeUrl(game.coverUrl)}</image:loc>
+      <image:title>${safeUrl(game.title)}</image:title>
+    </image:image>
   </url>\n`;
   });
 
   xml += `</urlset>`;
 
   fs.writeFileSync(SITEMAP_FILE, xml, 'utf8');
-  console.log(`Generated sitemap.xml with ${gamesData.length + 3} URLs!`);
+  console.log(`[SEO] Sitemap successfully generated with ${gamesData.length + 3} indexed URLs at ${SITEMAP_FILE}`);
 };
 
 generateSitemap();
